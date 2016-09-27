03191a556201110cd5fc2905e344734e8f80b8a1
jimmy.yang <whutycc@163.com>
2016-09-27 17:19:07 +0800

    change folder structure

---
+++ src/1GettingReady/learningc.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : learningc.c
+ Author      : jimmy
+ Version     :
+ Copyright   : Based on C Primer Plus
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void learningc(){
+	int num;
+	num = 1;
+	printf("I am a simple ");
+	printf("C program,\n");
+	printf("My favorite number is %d because it is the first one.\n", num);
+	printf("start learning c...");
+}
+
+//int main(void) {
+//	learningc();
+//	return EXIT_SUCCESS;
+//}
